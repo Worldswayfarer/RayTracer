7a3f796d82be325535a956429cc884db1f083c0b
Worldswayfarer <133384338+Worldswayfarer@users.noreply.github.com>
2024-11-02 18:58:50 +0100

    BVH construction

--- BVH/BoundingVolumeTree.h
+++ BVH/BoundingVolumeTree.h
@@ -2,10 +2,18 @@
 #include "AABB.h"
 #include "../Data/triangle3.h"
 
-struct BVHNode 
-{
+class BVHNode 
+{	public:
 	AABB* box = nullptr;
 	triangle3* primitive = nullptr;
 	BVHNode* left_child = nullptr;
 	BVHNode* right_child = nullptr;
+
+	BVHNode() {}
+	~BVHNode() {
+		delete box;
+		delete primitive;
+		delete left_child;
+		delete right_child;
+	}
 };
--- BVH/Construct_BVH.cpp
+++ BVH/Construct_BVH.cpp
@@ -1,6 +1,11 @@
 #include "Construct_BVH.h"
 #include <vector>
 #include <algorithm>
+#include <limits>
+#include <cmath>
+
+
+const int number_of_buckets = 3;
 
 bool compareX(triangle3& a, triangle3& b)
 {
@@ -18,49 +23,77 @@ bool compareZ(triangle3& a, triangle3& b)
 //sort primitives once at the start
 BVHNode* construct_BVH(std::vector<triangle3>* primitives)
 {
-    std::vector<triangle3>* x_sorted = primitives;
-    std::vector<triangle3>* y_sorted = primitives;
-    std::vector<triangle3>* z_sorted = primitives;
-
-    std::sort(x_sorted->begin(), x_sorted->end(), compareX);
-    std::sort(y_sorted->begin(), y_sorted->end(), compareY);
-    std::sort(z_sorted->begin(), z_sorted->end(), compareZ);
-
-
-
-    return nullptr;
+    return construct_node(nullptr,primitives);
 }
 
-//would be much less effiecient, 
-BVHNode* construct_node(std::vector<triangle3>* primitives, std::vector<triangle3>* x_sorted,
-    std::vector<triangle3>* y_sorted, std::vector<triangle3>* z_sorted)
-{
-    BVHNode* new_node = new BVHNode();
 
-    if (primitives->size() == 1)
-    {
-        new_node->primitive;
+BVHNode* construct_node(BVHNode* parent, std::vector<triangle3>* primitives)
+{
+    BVHNode* node = new BVHNode();
+
+    if (primitives->size() <= 1) {
+        node->primitive = &primitives->at(0);
+        return node;
     }
 
-    return nullptr;
-}
-
-BVHNode* construct_node(std::vector<triangle3>* primitives)
-{
-    BVHNode* new_node = new BVHNode();
-
-
-    if (primitives->size() == 1)
-    {
-        new_node->primitive;
+    AABB* parent_AABB;
+    if (!parent) {
+        parent_AABB = construct_AABB(primitives);
+    }
+    else {
+        parent_AABB = parent->box;
     }
 
-    return nullptr;
+    float bestCost = std::numeric_limits<float>::infinity();
+    size_t bestSplit = 0;
+    int bestAxis = -1;
+    
+    size_t primitives_per_bucket = primitives->size() / number_of_buckets;
+
+
+    for (int axis = 0; axis < 3; axis++) {
+        std::sort(primitives->begin(), primitives->end(), [axis](const triangle3& a, const triangle3& b) {
+            return (axis == 0 ? a.center().x() : (axis == 1 ? a.center().y() : a.center().z())) <
+                (axis == 0 ? b.center().x() : (axis == 1 ? b.center().y() : b.center().z()));
+            });
+        for (int bucket = 0; bucket < number_of_buckets; bucket++) {
+            std::vector<triangle3> left(primitives->begin(), primitives->begin() + bucket * primitives_per_bucket);
+            std::vector<triangle3> right(primitives->begin() + bucket * primitives_per_bucket, primitives->begin());
+
+            AABB* leftBox = construct_AABB(&left);
+            AABB* rightBox = construct_AABB(&right);
+            float sahCost = 1 + (compute_surface_area(leftBox) / compute_surface_area(parent_AABB)) * left.size() +
+                (compute_surface_area(rightBox) / compute_surface_area(parent_AABB)) * right.size();
+
+            if (sahCost < bestCost) {
+                bestCost = sahCost;
+                bestSplit = bucket;
+                bestAxis = axis;
+                node->left_child = construct_node(node, &left);
+                node->right_child = construct_node(node, &right);
+            }
+        }
+    }
+    return node;
 }
 
+AABB* construct_AABB(const std::vector<triangle3>* primitives) {
+    vector3 max = { std::numeric_limits<float>::lowest(), std::numeric_limits<float>::lowest(), std::numeric_limits<float>::lowest() };
+    vector3 min = {std::numeric_limits<float>::max(), std::numeric_limits<float>::max(), std::numeric_limits<float>::max()};
+    for (const triangle3 obj : *primitives) {
+        min = minimum(min, obj.a());
+        max = minimum(max, obj.a());
+        min = minimum(min, obj.b());
+        max = minimum(max, obj.b());
+        min = minimum(min, obj.c());
+        max = minimum(max, obj.c());
+    }
+    AABB box = { min, max };
+    return &box;
+}
 
-AABB* construct_AABB()
-{
-    return nullptr;
+float compute_surface_area(const AABB* box) {
+    vector3 d = { box->_max.x() - box->_min.x(), box->_max.y() - box->_min.y(), box->_max.z() - box->_min.z()};
+    return 2 * (d.x() * d.y() + d.y() * d.z() + d.z() * d.x());
 }
 
